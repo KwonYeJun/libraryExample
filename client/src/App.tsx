8673b0d272fa35912b8a585992edabd4e5b369c3
gwonyejun <kyjuni1059@gmail.com>
2023-07-11 17:59:37 +0900

    애미메이션 효과 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import './App.css';
 import CustomCard from './test';
 function App() {
@@ -7,14 +7,27 @@ function App() {
     <div className="App">
       <motion.div
         animate={{ rotate: 180 }}
-        transition={{ type: "spring", bounce: 0.25 ,stiffness: 50, velocity: 2}}
-        style={{backgroundColor: 'blue', width: '300px' , height: '300px' }}
+        transition={{ type: "spring", bounce: 0.25, stiffness: 50, velocity: 2 }}
+        drag
+        dragConstraints={{ left: 0, right: 1920 }}
+        dragElastic={0.2}
+        style={{ backgroundColor: 'blue', width: '300px', height: '300px' }}
       />
-  <CustomCard
-  image="/img/logo.png"
-  title="Card Title"
-  description="This is a card description."
-/>
+      <motion.div onHoverStart={() => console.log('Hover starts')}  style={{ backgroundColor: 'pink', width: '300px', height: '300px' }} />
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ type: "spring", bounce: 0.25, stiffness: 50, velocity: 2 }}
+        drag
+        dragConstraints={{ left: 0, right: 200 , top: -200 , bottom:200 }}
+        dragElastic={0.2}
+      >
+        <CustomCard
+          image="/img/logo.png"
+          title="Card Title"
+          description="This is a card description."
+        />
+      </motion.div>
+
     </div>
   );
 }
