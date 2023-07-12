0ccc980ad7967c654f5fa6ed2d47a090d41379a0
gwonyejun <kyjuni1059@gmail.com>
2023-07-12 14:07:58 +0900

    이미지 슬라이드 수정

--- client/src/Example.tsx
+++ client/src/Example.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { wrap } from "popmotion";
 import { images } from "./image-data";
+import CustomCard from './Crad';
 
 const variants = {
   enter: (direction: number) => {
@@ -25,12 +26,6 @@ const variants = {
   }
 };
 
-/**
- * Experimenting with distilling swipe offset and velocity into a single variable, so the
- * less distance a user has swiped, the more velocity they need to register as a swipe.
- * Should accomodate longer swipes and short flicks without having binary checks on
- * just distance thresholds and velocity > 0.
- */
 const swipeConfidenceThreshold = 10000;
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity;
@@ -39,10 +34,6 @@ const swipePower = (offset: number, velocity: number) => {
 export const Example = () => {
   const [[page, direction], setPage] = useState([0, 0]);
 
-  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
-  // then wrap that within 0-2 to find our image ID in the array below. By passing an
-  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
-  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
   const imageIndex = wrap(0, images.length, page);
 
   const paginate = (newDirection: number) => {
@@ -52,9 +43,8 @@ export const Example = () => {
   return (
     <>
       <AnimatePresence initial={false} custom={direction}>
-        <motion.img
+        <motion.div
           key={page}
-          src={images[imageIndex]}
           custom={direction}
           variants={variants}
           initial="enter"
@@ -76,7 +66,22 @@ export const Example = () => {
               paginate(-1);
             }
           }}
-        />
+        >
+          <motion.a
+            key={`card${page}`}
+            animate={{ rotate: [-180, -120, -60, 0], x: [-400, -200, 0], y: [-400, -200, 0], z: [-400, -200, 0] }}
+            className="box"
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 20, damping: 10, velocity: 2 }}
+          >
+            <CustomCard
+              image={images[imageIndex]}
+              title="Card Title"
+              description="This is a card description."
+              color="green"
+            />
+          </motion.a>
+        </motion.div>
       </AnimatePresence>
       <div className="next" onClick={() => paginate(1)}>
         {"‣"}
